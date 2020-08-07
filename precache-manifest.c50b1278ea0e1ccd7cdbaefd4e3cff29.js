ade7919ce8033d5cd2f3ccf9b15fb08b748a1622
Lisa <Lisa@lisa.comm>
2020-08-07 20:25:29 +0800

    12

---
+++ precache-manifest.c50b1278ea0e1ccd7cdbaefd4e3cff29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b494d9fdf75bb8ad555fbeaf564b7e4",
+    "url": "/Tomato/index.html"
+  },
+  {
+    "revision": "18b8788d1ded93a603b2",
+    "url": "/Tomato/static/css/main.12bf4958.chunk.css"
+  },
+  {
+    "revision": "4b7fd9e705d9100ef84a",
+    "url": "/Tomato/static/js/2.13cf2a11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tomato/static/js/2.13cf2a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b8788d1ded93a603b2",
+    "url": "/Tomato/static/js/main.d0451f71.chunk.js"
+  },
+  {
+    "revision": "72e99a251fb3970c8244",
+    "url": "/Tomato/static/js/runtime-main.7e37534d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6243572f44811ebb5040ec45eb57edd.js"
+  "/Tomato/precache-manifest.c50b1278ea0e1ccd7cdbaefd4e3cff29.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tomato/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
