9bdeb6da92e37bc2852a9f8a0aa1d7074db916b1
Lisa <Lisa@lisa.comm>
2020-08-07 16:49:38 +0800

    1
    1

---
+++ precache-manifest.a6243572f44811ebb5040ec45eb57edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea277c3a8342751bc28045a7871d735a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47b6a3aeadae26eca581",
+    "url": "/static/css/main.12bf4958.chunk.css"
+  },
+  {
+    "revision": "4b7fd9e705d9100ef84a",
+    "url": "/static/js/2.13cf2a11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13cf2a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b6a3aeadae26eca581",
+    "url": "/static/js/main.ae22abb9.chunk.js"
+  },
+  {
+    "revision": "0a967d5abe957ea20a7f",
+    "url": "/static/js/runtime-main.b297a5ca.js"
+  }
+]);
